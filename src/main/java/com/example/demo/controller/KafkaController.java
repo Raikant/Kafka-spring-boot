5c3fd071c10abdbc01696a1c85316486b9410783
ravikant <ravi.pathak918@gmail.com>
2020-01-04 14:50:28 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public KafkaController(KafkaTemplate<String, String> template) {
+		this.kafkaTemplate = template;
+	}
+
+	@GetMapping("/kafka/produce")
+	public void produce(@RequestParam String message) {
+		kafkaTemplate.send("myTopic", message);
+	}
+
+}
