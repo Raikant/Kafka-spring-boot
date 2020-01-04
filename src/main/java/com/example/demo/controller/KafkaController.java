9d16159adbedd398f13ffd1b016839c9d51d4475
ravikant <ravi.pathak918@gmail.com>
2020-01-04 15:11:19 +0530

    consumer added

--- src/main/java/com/example/demo/controller/KafkaController.java
+++ src/main/java/com/example/demo/controller/KafkaController.java
@@ -1,21 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.consumer.MyTopicConsumer;
+
 @RestController
 public class KafkaController {
 	private KafkaTemplate<String, String> kafkaTemplate;
+	private MyTopicConsumer myTopicConsumer;
 
-	public KafkaController(KafkaTemplate<String, String> template) {
+	public KafkaController(KafkaTemplate<String, String> template, MyTopicConsumer myTopicConsumer) {
 		this.kafkaTemplate = template;
+		this.myTopicConsumer = myTopicConsumer;
 	}
 
 	@GetMapping("/kafka/produce")
 	public void produce(@RequestParam String message) {
 		kafkaTemplate.send("myTopic", message);
+	}
+
+	@GetMapping("/kafka/messages")
+	public List<String> getMessages() {
+		return myTopicConsumer.getMessages();
 	}
 
 }
